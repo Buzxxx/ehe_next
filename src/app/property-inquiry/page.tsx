aa8b2dcab9a97af370e79ab0a9d0d96040b01444
grvx <spamgauravjha@gmail.com>
2024-05-09 00:35:23 +0530

    initial load

--- src/app/property-inquiry/page.tsx
+++ src/app/property-inquiry/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <>
       <div className="min-h-screen bg-blue-50">
-        <div className="flex justify-center md:pt-10">PreProperty form</div>
+        <div className="flex justify-center md:pt-10">PreProperty form Home </div>
       </div>
     </>
   );
