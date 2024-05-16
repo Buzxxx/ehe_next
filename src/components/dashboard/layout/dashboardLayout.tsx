1c853cb7aefe89a40232974242c97cb271ae1971
grvx <spamgauravjha@gmail.com>
2024-05-16 17:04:22 +0530

    dashboard minor changes

--- src/components/dashboard/layout/dashboardLayout.tsx
+++ src/components/dashboard/layout/dashboardLayout.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { ReactNode, useState } from "react"; // import "./globals.css";
+import React, { ReactNode, useState, useEffect } from "react"; // import "./globals.css";
 
 import DashboardSideMenu from "@/components/dashboard/feature/sideBar";
 import DashboardHeader from "@/components/dashboard/feature/dashboardHeader";
@@ -11,13 +11,32 @@ const DashboardLayout = ({ children }: { children: ReactNode }) => {
 
   const handleToggle = () => {
     setToggled(!toggled);
+    localStorage.setItem("toggled", JSON.stringify(!toggled));
   };
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const storedToggled = localStorage.getItem("toggled");
+
+      if (storedToggled !== null) {
+        setToggled(JSON.parse(storedToggled));
+      }
+    }
+  }, []);
+
+
 
   return (
     <div className="min-h-full">
-      <DashboardHeader onNext={handleToggle} />
+      <DashboardHeader toggled={toggled} onNext={handleToggle} />
       <DashboardSideMenu toggled={toggled} online={online} />
-      <div className={`${toggled ? "ml-64" : "ml-0"} transition-all duration-300 min-h-96 mt-20`}>{children}</div>
+      <div
+        className={`${
+          toggled ? "md:ml-64" : "ml-0"
+        } transition-all duration-300 min-h-96 mt-20`}
+      >
+        {children}
+      </div>
     </div>
   );
 };
