fbdc82c509991a43f386fcf3c581ae1190625b9e
grvx <spamgauravjha@gmail.com>
2024-08-12 16:34:29 +0530

    Add Login module

--- src/components/accounts/ui/login.tsx
+++ src/components/accounts/ui/login.tsx
@@ -1,3 +1,5 @@
+// /components/accounts/ui/login
+
 "use client"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
@@ -7,7 +9,9 @@ import { Button } from "@/components/ui/button"
 import { Form } from "@/components/ui/form"
 import InputField from "./inputField"
 import { useRouter, useSearchParams } from "next/navigation"
-import { Suspense, useState } from "react"
+import { useState } from "react"
+import Spinner from "@/components/ui/icons/spinner"
+import { loginUser } from "@/app/actions/auth.actions"
 
 // Ensure proper typing and schema
 const formSchema = z.object({
@@ -23,7 +27,7 @@ const Login = () => {
   const router = useRouter()
   const searchParams = useSearchParams()
   const redirectPath = searchParams.get("redirect") || "/lead" // Default redirect path if not set
-
+  const [isSubmitting, setIsSubmitting] = useState(false)
   const [message, setMessage] = useState({ status: "", message: "" })
 
   const form = useForm<z.infer<typeof formSchema>>({
@@ -35,22 +39,16 @@ const Login = () => {
   })
 
   async function onSubmit(values: z.infer<typeof formSchema>) {
+    setIsSubmitting(true)
     try {
-      const response = await fetch("/api/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(values),
-      })
+      const response = await loginUser(values)
 
-      if (!response.ok) {
-        throw new Error("Login failed")
+      if (!response) {
+        throw new Error("Username or Password Incorrect")
       }
 
-      const result = await response.json()
 
-      if (result.access) {
+      if (response.access) {
         setMessage({ status: "success", message: "Login successful" })
         form.reset()
         router.push(redirectPath)
@@ -60,17 +58,21 @@ const Login = () => {
           message: "Username or password incorrect",
         })
       }
+
+      setIsSubmitting(false)
     } catch (error) {
       setMessage({
         status: "failure",
         message: "Login failed",
       })
+      setIsSubmitting(false)
+
     }
   }
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="w-full text-sm">
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-full text-sm space-y-4">
         <InputField
           label="Username"
           placeholder="username"
@@ -84,20 +86,25 @@ const Login = () => {
         />
         <Button
           type="submit"
-          className="mt-4 bg-primary hover:bg-primary-hover py-6 w-full "
+          className={` py-6 h-5 w-full ${
+            !isSubmitting ? "bg-primary hover:bg-primary-hover" : "bg-secondary border border-primary"
+          } `}
+          disabled={isSubmitting}
         >
-          Login
+          {!isSubmitting ? "Login" : <Spinner  />}
         </Button>
 
-        {message.status && (
-          <div
-            className={`mt-2 text-center ${
-              message.status === "failure" ? "text-red-600" : "text-green-600"
-            }`}
-          >
-            {message.message}
-          </div>
-        )}
+        <div className="relative py-4">
+          {!isSubmitting && (
+            <div
+              className={` text-center absolute top-1/2 -translate-y-1/2 ${
+                message.status === "failure" ? "text-red-600" : "text-green-600"
+              }`}
+            >
+              {message.message}
+            </div>
+          )}
+        </div>
         <Link
           className=" mt-2 text-sm block"
           href="/accounts/resetPassword"
--- src/lib/loginUser.tsx
+++ src/lib/loginUser.tsx
@@ -1,37 +1,14 @@
-import { handleLogin } from "@/app/actions"
 
-interface LoginData {
-  username: string
-  password: string
-}
+// {
+//   code: 'token_not_valid',
+//   messages: [
+//   {
+//   token_class: 'AccessToken',
+//   token_type: 'access',
+//   message: 'Token is invalid or expired'
+// }
+// ],
+//   detail: 'Given token not valid for any token type'
+// }
 
-interface TokenResponse {
-  access: string
-  refresh: string
-}
-export const loginUser = async (data: LoginData): Promise<TokenResponse> => {
-  try {
-    const response = await fetch(
-      "https://www.eheindustries.com/auth/api/token",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      }
-    )
 
-    if (!response.ok) {
-      throw new Error("Network response was not ok")
-    }
-
-    const result: TokenResponse = await response.json()
-
-    handleLogin(result.access, result.refresh)
-    return result
-  } catch (error) {
-    console.error("Error logging in:", error)
-    throw new Error(`Error logging in: ${(error as Error).message}`)
-  }
-}
