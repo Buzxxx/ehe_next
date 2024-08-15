07e8d8b51d22045876271de65bc0cc57b49f52b8
grvx <spamgauravjha@gmail.com>
2024-08-15 21:04:43 +0530

    add form validation

---
+++ src/lib/validation.ts
@@ -0,0 +1,31 @@
+import { z } from "zod"
+
+export const leadShareValidation = z.object({
+  from: z
+    .string()
+    .min(2, "Name must be at least 2 characters")
+    .max(50, "Name must be at most 50 characters"),
+  to: z
+    .string()
+    .min(2, "Name must be at least 2 characters")
+    .max(50, "Name must be at most 50 characters"),
+  crm: z
+    .string()
+})
+
+
+const DateSchema = z.union([
+  z.string(), // For single date like "Today"
+  z.object({
+    start: z.string(), // Expecting ISO date string or similar
+    end: z.string(), // Expecting ISO date string or similar
+  }),
+])
+
+export const FilterFormSchema = z.object({
+  status: z.string(),
+  user: z.string(),
+  source: z.string(),
+  location: z.string(),
+  date: DateSchema,
+})
