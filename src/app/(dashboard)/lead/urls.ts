f16b0011b8ba604f992742bb6746eafd2d3001a0
grvx <spamgauravjha@gmail.com>
2024-09-05 18:10:53 +0530

    add lead urls

---
+++ src/app/(dashboard)/lead/urls.ts
@@ -0,0 +1,7 @@
+export const paths = {
+  AllLeads: "/lead",
+  CreateLead: "/lead/create",
+  LeadDetails: "/lead/:id",
+  LeadReassignModal: "/lead/leadReassignModal",
+  ShareLead: "/lead/share",
+}
