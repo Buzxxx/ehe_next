d0aebee1ddc272b104c50c39274bd98d727c520c
grvx <spamgauravjha@gmail.com>
2024-10-21 15:51:42 +0530

    add bookAppointment comp

--- src/components/propertyPage/features/bookAppointment.tsx
+++ src/components/propertyPage/features/bookAppointment.tsx
@@ -10,6 +10,8 @@ import { Form } from "@/components/ui/form"
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
 import { useForm } from "react-hook-form"
 import { ChevronDown, ChevronUp } from "lucide-react" // Import icons for chevron
+import Link from "next/link"
+import { Separator } from "@/components/ui/separator"
 
 // Appointment validation schema
 const AppointmentSchema = z.object({
@@ -38,12 +40,12 @@ const BookAppointment = () => {
     ? [...initialSlots, ...moreSlots]
     : initialSlots
 
-  // Estimate each slot's height (for example, 50px per slot)
-  const slotHeight = 80
+  // Estimate each slot's height (for example, 72px per slot)
+  const slotHeight = 60
 
   // Calculate max-height dynamically based on the number of displayed slots
   const maxHeight = useMemo(() => {
-    return slotsToDisplay.length * slotHeight
+    return (slotsToDisplay.length + 1) * slotHeight
   }, [slotsToDisplay.length])
 
   const form = useForm<z.infer<typeof AppointmentSchema>>({
@@ -55,12 +57,16 @@ const BookAppointment = () => {
   }
 
   return (
-    <article className="p-4 border border-gray-300/75 rounded-lg shadow-md">
+    <article className="px-4 pb-4 border border-gray-300/75 rounded-lg shadow-md flex-1">
       {/* Property Info */}
-      <hgroup>
-        <h4 className="text-xl font-semibold">Microtek Greenburg</h4>
-        <span className="text-gray-500 text-sm">Gurugram, Harayana</span>
+      <hgroup className="py-2">
+        <h4 className="text-3xl text-slate-800 font-bold leading-8">
+          Microtek Greenburg
+        </h4>
+        <span className="text-gray-500 text-sm">Gurugram, Haryana</span>
       </hgroup>
+
+      <Separator />
 
       {/* High Demand Notification */}
       <div className="bg-orange-100 py-2 px-4 rounded-md mt-4 flex gap-2 items-center">
@@ -72,13 +78,17 @@ const BookAppointment = () => {
 
       {/* Appointment Form */}
       <Form {...form}>
-        <h4 className="text-md font-medium mt-6">Book An Appointment</h4>
+        <h4 className="text-md font-medium mt-4">Book An Appointment</h4>
         <p className="text-xs text-slate-600">Book your appointment online</p>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 p-4">
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 py-4 px-6">
           {/* Time Slot Selection */}
           <div
             className="transition-all duration-500 overflow-hidden"
-            style={{ height: `${maxHeight}px` }} // Dynamic max-height
+            style={{
+              height: `${
+                showMoreSlots ? `calc(${maxHeight}px + 80px)` : `${maxHeight}px`
+              }`,
+            }} // Dynamic max-height
           >
             <RadioGroup
               className="space-y-3"
@@ -87,7 +97,7 @@ const BookAppointment = () => {
               {slotsToDisplay.map((slot, index) => (
                 <div
                   key={index}
-                  className={`flex items-center space-x-2 transition `}
+                  className={`flex items-center transition `}
                 >
                   <RadioGroupItem
                     value={slot}
@@ -111,7 +121,7 @@ const BookAppointment = () => {
               <Button
                 type="button"
                 variant="outline"
-                className="w-full flex justify-center items-center text-slate-500 border-slate-300 hover:text-slate-700 hover:border-slate-500"
+                className="w-full flex justify-center items-center text-slate-500 border-slate-300 hover:text-slate-700 hover:border-slate-500 py-2 h-fit"
                 onClick={() => setShowMoreSlots(!showMoreSlots)}
               >
                 {showMoreSlots ? "See Less Slots" : "Show More Slots"}
@@ -121,6 +131,25 @@ const BookAppointment = () => {
                   <ChevronDown className="ml-2" />
                 )}
               </Button>
+
+              {/* Couldn't find the right slot message */}
+              {showMoreSlots && (
+                <p className="text-slate-600 text-xs font-medium text-balance">
+                  Couldn&apos;t find the right slot for you?
+                  <Link
+                    href="#contact"
+                    className="text-blue-500 font-semibold hover:underline"
+                  >
+                    {" "}
+                    We&apos;re here to help!
+                  </Link>
+                  <br />
+                  <span className="text-slate-500 font-normal text-center">
+                    Get in touch with us, and we&apos;ll assist you in finding a
+                    more convenient time.
+                  </span>
+                </p>
+              )}
             </RadioGroup>
           </div>
 
