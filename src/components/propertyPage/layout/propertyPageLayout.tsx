8f14ccdf556af2769d2a40024d71e85ff29f6d64
grvx <spamgauravjha@gmail.com>
2024-10-21 15:53:45 +0530

    Add Property Page layout

--- src/components/propertyPage/layout/propertyPageLayout.tsx
+++ src/components/propertyPage/layout/propertyPageLayout.tsx
@@ -1,28 +1,35 @@
-import Navbar from "@/components/propertyPage/ui/navbar"
+import Navbar from "@/components/propertyPage/features/navbar"
 import Hero from "../features/hero"
-import PropertyDetails from "../features/propertyDetails"
-
-const navLinks = [
-  { label: "Home", href: "/" },
-  { label: "About Us", href: "/about" },
-  { label: "Properties", href: "/properties" },
-  { label: "Agents", href: "/agents" },
-  { label: "Blog", href: "/blog" },
-  { label: "Contact", href: "/contact" },
-]
-
-const user = {
-  isLoggedIn: false,
-  avatarUrl: "",
-}
+import PropertyListingSection from "../features/propertyListingSection"
+import BookAppointment from "../features/bookAppointment"
+import PropertyStatsOverview from "../features/propertyStatsOverview"
+import PropertyAbout from "../ui/propertyAbout"
+import PropertyForm from "@/components/propertyPage/features/propertyForm"
+import PropertyFeaturesTable from "../features/propertyFeaturesTable"
+import PropertyListerInfo from "../ui/propertyListerInfo"
 
 const PropertyPageLayout = () => {
   return (
     <>
-      <Navbar logo="/property/icons/logo.svg" navLinks={navLinks} user={user} />
+      <Navbar />
       <Hero />
-      <PropertyDetails />
-      
+      <section className="flex md:flex-row flex-col-reverse md:gap-2 gap-4 md:px-20 px-4">
+        <div className="flex flex-col md:gap-0 gap-4 md:w-2/3">
+          <PropertyStatsOverview />
+          <PropertyAbout />
+        </div>
+        <BookAppointment />
+      </section>
+
+      <section className="flex md:flex-row flex-col gap-4 md:px-20 px-4">
+        <div className="flex flex-col gap-4 md:w-2/3">
+          <PropertyFeaturesTable />
+          <PropertyListerInfo />
+        </div>
+        <PropertyForm />
+      </section>
+
+      <PropertyListingSection />
     </>
   )
 }
