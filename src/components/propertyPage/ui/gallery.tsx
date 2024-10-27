f95e72631509eca4c4f0017f97fff539e5fee4a4
grvx <spamgauravjha@gmail.com>
2024-10-27 12:55:00 +0530

    add carousel gallery

--- src/components/propertyPage/ui/gallery.tsx
+++ src/components/propertyPage/ui/gallery.tsx
@@ -8,84 +8,69 @@ import { Button } from "@/components/ui/button"
 import Image, { StaticImageData } from "next/image"
 
 import { Images } from "@/components/ui/icons"
+import { useState } from "react"
+import ModalCarousel from "./modalCarousel"
 
-const Gallery = ({ images }: { images: string | StaticImageData[] }) => {
-  // Function to handle 'View All Photos' click
-  const viewAllPhotos = () => {
-    console.log("View All Photos clicked!")
+const Gallery = ({ images }: { images: (string | StaticImageData)[] }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [focusedImageIndex, setFocusedImageIndex] = useState(0)
+
+  const openModalWithImage = (index: number) => {
+    setFocusedImageIndex(index)
+    setIsModalOpen(true)
   }
 
   return (
-    <div className="md:w-1/3 grid md:grid-cols-2 grid-cols-4 md:grid-rows-4 gap-2 ">
-      {images[0] && (
-        <div className="col-span-2 row-span-2">
+    <div className="md:w-1/3 grid md:grid-cols-2 grid-cols-4 md:grid-rows-4 gap-2  ">
+      {images.map((image, index) => (
+        <div
+          key={index}
+          className={`${
+            index === 0
+              ? "col-span-2 row-span-2"
+              : index === 1
+              ? "md:col-span-1 col-span-2 md:row-span-2 row-span-1"
+              : "row-span-1"
+          } ${index === 3 ? "relative" : ""}`}
+          onClick={() => openModalWithImage(index)}
+        >
           <Image
             placeholder="blur"
-            src={images[0]}
-            alt="Gallery Image 1"
-            width={400}
-            height={400}
+            src={image}
+            alt={`Gallery Image ${index + 1}`}
+            width={index === 0 ? 400 : 200}
+            height={index < 2 ? 400 : 200}
             className="rounded-lg object-cover h-full w-full"
           />
+          {index === 3 && (
+            <>
+              <Button
+                onClick={() => openModalWithImage(0)}
+                className="absolute bottom-2 right-2 w-fit rounded-lg bg-slate-800 hover:bg-slate-900 text-slate-200 text-xs h-fit py-3 gap-2 shadow-inner border border-slate-400/70 hidden md:flex"
+              >
+                <Images size={16} />
+                View All Photos
+              </Button>
+              <div
+                onClick={() => openModalWithImage(0)}
+                className="absolute w-full h-full top-0 left-0 rounded-lg bg-slate-800/75 hover:bg-slate-800/70 transition text-slate-200 text-lg font-semibold md:hidden flex items-center justify-center gap-1.5"
+              >
+                <Images size={16} />{" "}
+                <p className="flex">
+                  <span className="text-xl">+ </span> {images.length}
+                </p>
+              </div>
+            </>
+          )}
         </div>
-      )}
-
-      {images[1] && (
-        <div className="md:col-span-1 col-span-2 md:row-span-2 row-span-1">
-          <Image
-            placeholder="blur"
-            src={images[1]}
-            alt="Gallery Image 2"
-            width={200}
-            height={400}
-            className="rounded-lg object-cover h-full w-full"
-          />
-        </div>
-      )}
-
-      {images[2] && (
-        <div className="row-span-1">
-          <Image
-            placeholder="blur"
-            src={images[2]}
-            alt="Gallery Image 3"
-            width={200}
-            height={200}
-            className="rounded-lg h-full w-full"
-          />
-        </div>
-      )}
-
-      {images[3] && (
-        <div className="relative">
-          <Image
-            placeholder="blur"
-            src={images[3]}
-            alt="Gallery Image 4"
-            width={200}
-            height={200}
-            className="rounded-lg object-cover h-full w-full"
-          />
-
-          {/* Desktop Screen Button */}
-          <Button
-            onClick={viewAllPhotos}
-            className="absolute bottom-2 right-2 w-fit rounded-lg bg-slate-800 hover:bg-slate-900 text-slate-200 text-xs h-fit py-3 gap-2 shadow-inner border border-slate-400/70 hidden md:flex"
-          >
-            <Images size={16} />
-            View All Photos
-          </Button>
-
-          <div
-            onClick={viewAllPhotos}
-            className="absolute w-full h-full top-0 left-0  rounded-lg bg-slate-800/75 hover:bg-slate-800/70 transition text-slate-200 text-lg font-semibold md:hidden flex items-center justify-center gap-1.5"
-          >
-            <Images size={16} />{" "}
-            <p className="flex">
-              <span className="text-xl">+ </span> {images.length}
-            </p>
-          </div>
-        </div>
+      ))}
+      {isModalOpen && (
+        <ModalCarousel
+          isOpen={isModalOpen}
+          setIsOpen={setIsModalOpen}
+          items={images}
+          initialIndex={focusedImageIndex}
+        />
       )}
     </div>
   )
