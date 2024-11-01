b86dbd6f1b9cf4a4331c8014d337e13196d6aa37
grvx <spamgauravjha@gmail.com>
2024-11-01 16:44:01 +0530

    add vendor auth

---
+++ src/app/contracts/vendor/auth/page.tsx
@@ -0,0 +1,9 @@
+/**
+ * @path src/app/contracts/vendor/auth/page.tsx
+ */
+
+import AuthLayout from "@/components/contracts/layout/authLayout"
+
+export default function Login() {
+  return <AuthLayout />
+}
