d7aa1e76f4502e48e0b722d8fac89ad0decdf57e
grvx <spamgauravjha@gmail.com>
2024-11-24 01:31:44 +0530

    lead profile improv

--- src/components/lead/features/leadPage/leadPageHeader.tsx
+++ src/components/lead/features/leadPage/leadPageHeader.tsx
@@ -62,40 +62,12 @@ const LeadPageHeader = ({
 
   return (
     <section className="p-2 md:p-4 md:pt-2 md:pb-0 pb-0 bg-white shadow-sm rounded-lg">
-      <header className="flex justify-between items-center md:mb-4 mt-8 md:mt-0">
+      <header className="flex justify-between items-center mt-10 md:mb-1 md:mt-0">
         <div className="flex items-center gap-2">
           <BackIcon
             onClick={() => router.back()}
-            className="p-1 cursor-pointer hover:bg-gray-200 rounded-full"
+            className="p-1 cursor-pointer hover:bg-gray-200 rounded-full h-fit py-0"
           />
-          <h1 className="text-lg font-semibold text-gray-800">Lead Details</h1>
-          {activeTab === 1 && (
-            <div className="flex items-center gap-2 ml-2">
-              {isEditing ? (
-                <>
-                  <Button
-                    onClick={handleSave}
-                    className="h-fit bg-transparent border text-sky-600 hover:text-sky-700 py-1 border-sky-600 hover:border-sky-700 hover:bg-transparent"
-                  >
-                    Save
-                  </Button>
-                  <Button
-                    onClick={handleCancel}
-                    className="h-fit bg-transparent border text-gray-500 hover:text-gray-700 py-1 border-gray-500 hover:border-gray-700 hover:bg-transparent"
-                  >
-                    Cancel
-                  </Button>
-                </>
-              ) : (
-                <Button
-                  onClick={() => setIsEditing(true)}
-                  className="h-fit bg-transparent border text-slate-400 hover:text-slate-600 py-1 border-slate-400 hover:border-slate-600 hover:bg-transparent"
-                >
-                  <Edit /> Edit
-                </Button>
-              )}
-            </div>
-          )}
         </div>
         <div className="flex items-center gap-2">
           <Button
@@ -119,47 +91,77 @@ const LeadPageHeader = ({
         </div>
       </header>
       <div className="flex flex-col md:flex-row items-start md:items-center justify-between">
-        <div className="flex items-center gap-4">
+        <div className="flex items-start gap-3 ml-2">
           <Avataar
             src={leadProfile.img ?? "/base/profile.webp"}
-            className="h-16 w-16 rounded-full border border-sky-800 p-1"
+            className="h-20 w-20 rounded-full border border-sky-800 p-1"
           />
-          <div className="flex flex-col gap-1">
+          <div className="flex flex-col 1">
             <EditableField
               value={localLeadProfile.name}
               fieldKey="name"
-              textSize="xl"
+              textSize="2xl"
               fontWeight="semibold"
             />
-            <div className="flex gap-2 items-center text-gray-500 text-sm">
-              <Mail size={16} />
-              <EditableField
-                value={localLeadProfile.email}
-                fieldKey="email"
-                textSize="sm"
-              />
-            </div>
-            <div className="flex gap-2 items-center text-gray-500 text-sm">
-              <Phone size={16} />
-              <EditableField
-                value={localLeadProfile.contact}
-                fieldKey="contact"
-                textSize="sm"
-              />
-              <span className="text-gray-300 h-4 overflow-hidden my-auto">
-                |
-              </span>
-              <BriefCase size={16} />
-              <EditableField
-                value={localLeadProfile.company}
-                fieldKey="company"
-                textSize="sm"
-              />
+            <div>
+              <div className="flex gap-1 items-center text-gray-500 text-sm">
+                <Mail size={16} />
+                <EditableField
+                  value={localLeadProfile.email}
+                  fieldKey="email"
+                  textSize="sm"
+                />
+              </div>
+              <div className="flex gap-1 items-center text-gray-500 text-sm">
+                <Phone size={16} />
+                <EditableField
+                  value={localLeadProfile.contact}
+                  fieldKey="contact"
+                  textSize="sm"
+                />
+                <span className="text-gray-300 h-4 overflow-hidden my-auto px-1">
+                  |
+                </span>
+                <BriefCase size={16} />
+                <EditableField
+                  value={localLeadProfile.company}
+                  fieldKey="company"
+                  textSize="sm"
+                />
+              </div>
             </div>
           </div>
         </div>
+
+        {activeTab === 1 && (
+          <div className="hidden md:flex items-center gap-2 ">
+            {isEditing ? (
+              <>
+                <Button
+                  onClick={handleSave}
+                  className="h-fit bg-transparent border text-sky-600 hover:text-sky-700 py-1 border-sky-600 hover:border-sky-700 hover:bg-transparent"
+                >
+                  Save
+                </Button>
+                <Button
+                  onClick={handleCancel}
+                  className="h-fit bg-transparent border text-gray-500 hover:text-gray-700 py-1 border-gray-500 hover:border-gray-700 hover:bg-transparent"
+                >
+                  Cancel
+                </Button>
+              </>
+            ) : (
+              <Button
+                onClick={() => setIsEditing(true)}
+                className="h-fit bg-transparent border text-slate-400 hover:text-slate-600 py-1 border-slate-400 hover:border-slate-600 hover:bg-transparent"
+              >
+                <Edit /> Edit
+              </Button>
+            )}
+          </div>
+        )}
       </div>
-      <nav className="md:pt-2 md:mt-2 flex items-center justify-start bg-white w-full">
+      <nav className="md:pt-2 mt-2 flex items-center justify-start bg-white w-full">
         {navItems.map((item, index) => (
           <button
             key={item.name}
@@ -173,6 +175,34 @@ const LeadPageHeader = ({
             {item.name}
           </button>
         ))}
+
+        {activeTab === 1 && (
+          <div className="md:hidden flex items-center gap-2 self-center ml-auto">
+            {isEditing ? (
+              <>
+                <Button
+                  onClick={handleSave}
+                  className="h-fit bg-transparent border text-sky-600 hover:text-sky-700 py-1 border-sky-600 hover:border-sky-700 hover:bg-transparent"
+                >
+                  Save
+                </Button>
+                <Button
+                  onClick={handleCancel}
+                  className="h-fit bg-transparent border text-gray-500 hover:text-gray-700 py-1 border-gray-500 hover:border-gray-700 hover:bg-transparent"
+                >
+                  Cancel
+                </Button>
+              </>
+            ) : (
+              <Button
+                onClick={() => setIsEditing(true)}
+                className="h-fit bg-transparent border text-slate-400 hover:text-slate-600 py-1 border-slate-400 hover:border-slate-600 hover:bg-transparent"
+              >
+                <Edit /> Edit
+              </Button>
+            )}
+          </div>
+        )}
       </nav>
     </section>
   )
