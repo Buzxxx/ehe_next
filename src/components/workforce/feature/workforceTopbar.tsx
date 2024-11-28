8498a08205e63e840588191e90c15054e140561b
grvx <spamgauravjha@gmail.com>
2024-11-28 19:21:55 +0530

    workforce updates

--- src/components/workforce/feature/workforceTopbar.tsx
+++ src/components/workforce/feature/workforceTopbar.tsx
@@ -8,15 +8,20 @@ import { Filter, Plus } from "@/components/ui/icons"
 
 const WorkplaceTopbar = () => {
   return (
-    <div className="flex justify-between items-center mt-4 border-b py-4">
-      <div className="flex gap-2 items-center">
-        <h4 className="text-xl font-medium">Employees </h4>
-        <Badge className="bg-orange-600/70 hover:bg-orange-500/70">12 New</Badge>{" "}
+    <div className="flex justify-between items-center  pb-4 pt-2 px-2 md:px-0">
+      <div className="flex gap-2 items-center md:flex-row flex-col">
+        <h4 className="text-xl font-medium">Teams </h4>
+        <Badge className="bg-orange-600/70 hover:bg-orange-500/70">
+          12 New
+        </Badge>{" "}
       </div>
       <div className="flex gap-2 items-center">
-        <Button variant="outline" className="border-2">
+        <Button
+          variant="outline"
+          className="md:border-2 bg-transparent border-0 md:bg-white"
+        >
           <Filter strokeWidth="1.5" />
-          Filters
+          <p className="hidden md:block">Filters</p>
         </Button>
         <Button className="bg-sky-600 hover:bg-sky-500">
           <Plus />
--- src/components/workforce/ui/workCard.tsx
+++ src/components/workforce/ui/workCard.tsx
@@ -8,34 +8,47 @@ import { EllipsisVertical, Mail, Phone } from "@/components/ui/icons"
 import { Input } from "@/components/ui/input"
 import Link from "next/link"
 
-export const WorkCard = () => (
-  <TableRow className="rounded-none">
-    <TableCell>
-      <Link href={`/workforce/${1}`} className="flex gap-2 items-center">
-        <Input type="checkbox" className="h-fit w-fit" />
+export const WorkCard = ({
+  name,
+  email,
+  phone,
+}: {
+  name: string
+  email: string
+  phone: string
+}) => (
+  <TableRow className="border-0 py-0 h-fit whitespace-nowrap">
+    <TableCell className="min-w-[200px]">
+      <Link
+        href={`/workforce/${1}`}
+        className="flex gap-2 items-center flex-nowrap truncate"
+      >
+        <Input type="checkbox" className="h-fit w-fit shrink-0" />
         <Avataar />
-        <div>
-          <h6 className="font-medium text-base">John Doe</h6>
-          <p className="text-gray-500 text-sm">Cheif Technical Officer</p>
+        <div className="truncate">
+          <h6 className="font-medium text-base truncate">{name}</h6>
+          <p className="text-gray-500 text-sm truncate">
+            Chief Technical Officer
+          </p>
         </div>
       </Link>
     </TableCell>
-    <TableCell>
-      <h6>Employee</h6>
+    <TableCell className="min-w-[150px] text-sm">
+      <h6 className="truncate">Employee</h6>
     </TableCell>
-    <TableCell>
-      <div className="flex gap-2 items-center">
+    <TableCell className="min-w-[200px]">
+      <div className="flex gap-2 items-center flex-nowrap truncate">
         <Mail color="gray" size={16} />
-        <h6>example@user.com</h6>
+        <h6 className="truncate">{email}</h6>
       </div>
     </TableCell>
-    <TableCell className=" gap-2 items-center">
-      <div className="flex gap-2 items-center">
+    <TableCell className="min-w-[150px]">
+      <div className="flex gap-2 items-center flex-nowrap truncate">
         <Phone color="gray" size={16} />
-        <h6>+91 900000000</h6>
+        <h6 className="truncate">{phone}</h6>
       </div>
     </TableCell>
-    <TableCell>
+    <TableCell className="min-w-[50px] text-center">
       <button>
         <EllipsisVertical />
       </button>
