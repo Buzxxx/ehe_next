c8bc26f0afb5b8a23d7dfadc59e68a72f3403bc1
grvx <spamgauravjha@gmail.com>
2024-11-29 17:29:14 +0530

    sidebar component ui improve

--- src/components/account/layout/accountLayout.tsx
+++ src/components/account/layout/accountLayout.tsx
@@ -33,17 +33,17 @@ export default function AccountLayout() {
   ]
   return (
     <>
-      <div className="flex items-center pt-2 pb-3 mb-4 border-b justify-between">
+      <div className="flex items-center pt-2 pb-3 mb-4 border-b justify-between px-2 md:px-0">
         <div className="flex items-center justify-between gap-2 ">
           <Avataar
             className="border-2 h-12 w-12 aspect-square"
             src="/logo.svg"
           />
           <div className="flex flex-col">
-            <h2 className="text-2xl font-semibold text-gray-800 leading-6">
+            <h2 className="md:text-2xl text-xl font-semibold text-gray-800 leading-6">
               Ehe_Industries
             </h2>
-            <p className="text-gray-500">Welcome to the admin console.</p>
+            <p className="text-gray-500 text-sm md:text-base">Welcome to the admin console.</p>
           </div>
         </div>
         <Button className="bg-sky-500 hover:bg-sky-600">
--- src/components/account/ui/entityCard.tsx
+++ src/components/account/ui/entityCard.tsx
@@ -24,12 +24,12 @@ const EntityCard = ({
       {/* Header */}
       <CardHeader className="flex-row justify-between items-start mb-2">
         <div className="flex flex-col text-balance flex-1">
-          <CardTitle className="text-xl text-gray-600">
+          <CardTitle className="md:text-xl text-lg text-gray-600">
             <Link href={`/account/${name}`}> {name} </Link>
           </CardTitle>
-          <p className="text-sm text-gray-500">{description}</p>
+          <p className="md:text-sm text-xs text-gray-500">{description}</p>
         </div>
-        <button className="text-base transition-all font-semibold text-sky-500 hover:text-sky-800">
+        <button className="md:text-base text-sm transition-all font-semibold text-sky-500 hover:text-sky-800">
           <Link href={`/account/${name}`}>Manage</Link>
         </button>
       </CardHeader>
@@ -37,15 +37,15 @@ const EntityCard = ({
       {/* Content */}
       <CardContent className="flex justify-between divide-x divide-gray-200 gap-2">
         <div className="flex flex-col items-center justify-center flex-1 ">
-          <p className="text-gray-500 text-sm">Total</p>
+          <p className="text-gray-500 md:text-sm text-xs">Total</p>
           <p className="text-gray-600 font-semibold">{totalEmployees}</p>
         </div>
         <div className="flex flex-col items-center justify-center flex-1 ">
-          <p className="text-gray-500 text-sm">Active</p>
+          <p className="text-gray-500 md:text-sm text-xs">Active</p>
           <p className="text-gray-600 font-semibold">{activeEmployees}</p>
         </div>
         <div className="flex flex-col items-center justify-center flex-1 ">
-          <p className="text-gray-500 text-sm">Inactive</p>
+          <p className="text-gray-500 md:text-sm text-xs">Inactive</p>
           <p className="text-gray-600 font-semibold">
             {totalEmployees - activeEmployees}
           </p>
@@ -56,19 +56,19 @@ const EntityCard = ({
       <CardFooter className="pt-4 flex-col items-start gap-4 ">
         <Link
           href={"#"}
-          className="text-sm font-medium text-slate-400 hover:text-sky-600"
+          className="md:text-sm text-xs font-medium text-slate-400 hover:text-sky-600"
         >
           Add User
         </Link>
         <Link
           href={"#"}
-          className="text-sm font-medium  text-slate-400 hover:text-sky-600"
+          className="md:text-sm text-xs font-medium  text-slate-400 hover:text-sky-600"
         >
           Delete User
         </Link>
         <Link
           href={"#"}
-          className="text-sm font-medium text-slate-400 hover:text-sky-600"
+          className="md:text-sm text-xs font-medium text-slate-400 hover:text-sky-600"
         >
           View Reports
         </Link>
