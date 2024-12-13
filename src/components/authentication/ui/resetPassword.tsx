e76937758ae6a93e61591e9813c4e39df313da3a
grvx <spamgauravjha@gmail.com>
2024-12-13 16:05:57 +0530

    reset password

--- src/components/authentication/ui/resetPassword.tsx
+++ src/components/authentication/ui/resetPassword.tsx
@@ -1,21 +1,22 @@
-"use client";
+"use client"
 
-import { z } from "zod";
-import { Form } from "@/components/ui/form";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Button } from "@/components/ui/button";
+import { z } from "zod"
+import { Form } from "@/components/ui/form"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Button } from "@/components/ui/button"
 
 // Import dynamic from next/dynamic and InputFieldPlaceholder
-import dynamic from "next/dynamic";
-import InputFieldPlaceholder from "@/components/authentication/ui/inputFieldPlaceholder";
+import dynamic from "next/dynamic"
+import InputFieldPlaceholder from "@/components/authentication/ui/inputFieldPlaceholder"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 
 const InputField = dynamic(
   () => import("@/components/authentication/ui/inputField"),
   {
     loading: () => <InputFieldPlaceholder />,
   }
-);
+)
 
 const formSchema = z
   .object({
@@ -35,7 +36,7 @@ const formSchema = z
   .refine((data) => data.newPassword === data.confirmPassword, {
     message: "Passwords do not match",
     path: ["confirmPassword"],
-  });
+  })
 
 const ResetPassword = () => {
   const form = useForm<z.infer<typeof formSchema>>({
@@ -45,61 +46,70 @@ const ResetPassword = () => {
       newPassword: "",
       confirmPassword: "",
     },
-  });
+  })
 
-  const onSubmit = (values: z.infer<typeof formSchema>) => {};
+  const onSubmit = (values: z.infer<typeof formSchema>) => {}
 
   const handleCancel = () => {
-    form.reset();
-  };
+    form.reset()
+  }
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="w-full">
-        <InputField
-          label="Old Password"
-          placeholder="old password"
-          field={form.register("oldPassword")}
-          isPassword={true}
-        />
-        <InputField
-          label="New Password"
-          placeholder="new password"
-          field={form.register("newPassword")}
-          isPassword={true}
-        />
-        <InputField
-          label="Confirm New Password"
-          placeholder="confirm new password"
-          field={form.register("confirmPassword")}
-          isPassword={true}
-        />
+    <Card className="max-w-lg mx-auto">
+      <CardHeader className="py-4">
+        <CardTitle className="text-center font-semibold md:text-2xl text-lg">
+          RESET PASSWORD
+        </CardTitle>
+      </CardHeader>
+      <CardContent className="pt-0">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="w-full">
+            <InputField
+              label="Old Password"
+              placeholder="old password"
+              field={form.register("oldPassword")}
+              isPassword={true}
+            />
+            <InputField
+              label="New Password"
+              placeholder="new password"
+              field={form.register("newPassword")}
+              isPassword={true}
+            />
+            <InputField
+              label="Confirm New Password"
+              placeholder="confirm new password"
+              field={form.register("confirmPassword")}
+              isPassword={true}
+            />
 
-        <div className="mt-4 text-slate-400 space-y-2 text-sm">
-          <p>New password must contain:</p>
-          <ul className="md:text-sm text-xs">
-            <li>- At least 8 characters</li>
-            <li>- At least 1 lowercase letter (a-z)</li>
-            <li>- At least 1 uppercase letter (A-Z)</li>
-            <li>- At least 1 number (0-9)</li>
-            <li>- At least 1 special character</li>
-          </ul>
-        </div>
-        <div className="gap-2 w-fit flex flex-row justify-between mt-4 h-10 items-center">
-          <Button type="submit" className="btn-primary">
-            Change
-          </Button>
-          <Button
-            type="button"
-            className="btn-revert bg-transparent hover:bg-muted"
-            onClick={handleCancel}
-          >
-            Cancel
-          </Button>
-        </div>
-      </form>
-    </Form>
-  );
-};
+            <div className="mt-4 text-slate-400 space-y-2 text-sm">
+              <p>New password must contain:</p>
+              <ul className="md:text-sm text-xs">
+                <li>- At least 8 characters</li>
+                <li>- At least 1 lowercase letter (a-z)</li>
+                <li>- At least 1 uppercase letter (A-Z)</li>
+                <li>- At least 1 number (0-9)</li>
+                <li>- At least 1 special character</li>
+              </ul>
+            </div>
+            <div className="gap-2 w-fit flex flex-row justify-between mt-4 h-10 items-center">
+              <Button type="submit" className="bg-sky-600 hover:bg-sky-700">
+                Change
+              </Button>
+              <Button
+                type="button"
+                className="text-gray-600 border border-gray-400 bg-transparent hover:bg-muted"
+                onClick={handleCancel}
+              >
+                Cancel
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
+  )
+}
 
-export default ResetPassword;
+export default ResetPassword
